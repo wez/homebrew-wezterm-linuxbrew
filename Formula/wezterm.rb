28dfd0979e87576e7f07f8e864a2e1bef7d5ce28
Wez Furlong <wez@wezfurlong.org>
2020-07-05 09:37:04 -0700

    manually update for head install and current release
    refs: https://github.com/wez/wezterm/issues/239

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -1,8 +1,9 @@
 class Wezterm < Formula
   desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
   homepage "https://wezfurlong.org/wezterm/"
-  url "https://github.com/wez/wezterm/releases/download/20200517-122836-92c201c6/WezTerm-20200517-122836-92c201c6-Ubuntu16.04.AppImage"
-  sha256 "dc53150749ef5e122270ac3ad194a055b614d92cb719cba0faed3e30e9e9e2da"
+  url "https://github.com/wez/wezterm/releases/download/20200620-160318-e00b076c/WezTerm-20200620-160318-e00b076c-Ubuntu16.04.AppImage"
+  sha256 "0ece46be9eb80b29cfea48331bf41823c7bce0eb06be978605e744a9e9fac78f"
+  head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu16.04.AppImage"
 
   def install
     Dir.glob("*.AppImage").each do |img|
