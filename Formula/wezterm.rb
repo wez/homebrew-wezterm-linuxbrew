6bf4a9899ac7258a20eed8f55db9230cbd4b501c
wez <wez@users.noreply.github.com>
2022-09-05 21:23:59 +0000

    Automated update to match latest tag

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -5,8 +5,8 @@
 class Wezterm < Formula
   desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
   homepage "https://wezfurlong.org/wezterm/"
-  url "https://github.com/wez/wezterm/releases/download/20220904-064125-9a6cee2b/WezTerm-20220904-064125-9a6cee2b-Ubuntu18.04.AppImage"
-  sha256 "664d0c61f1ccc931b25eb12e4bcd844e28bc6c4084c199fa26788371196eeebc"
+  url "https://github.com/wez/wezterm/releases/download/20220905-102802-7d4b8249/WezTerm-20220905-102802-7d4b8249-Ubuntu18.04.AppImage"
+  sha256 "ff7ce94c246a5c016b1b0a22edeb0c0d40ba019553b2cccf9ac060373a984697"
   head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu18.04.AppImage"
 
   def install
