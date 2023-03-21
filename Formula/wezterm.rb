73a6ce614dd3bec612e74378ad7bcb5a1597da13
Wez Furlong <wez@wezfurlong.org>
2023-03-21 07:53:04 -0700

    fix URL

--- Formula/wezterm.rb
+++ Formula/wezterm.rb
@@ -5,9 +5,9 @@
 class Wezterm < Formula
   desc "A GPU-accelerated cross-platform terminal emulator and multiplexer written by @wez and implemented in Rust"
   homepage "https://wezfurlong.org/wezterm/"
-  url "https://github.com/wez/wezterm/releases/download/20230320-124340-559cb7b0/WezTerm-20230320-124340-559cb7b0-Ubuntu18.04.AppImage"
+  url "https://github.com/wez/wezterm/releases/download/20230320-124340-559cb7b0/WezTerm-20230320-124340-559cb7b0-Ubuntu20.04.AppImage"
   sha256 "d0e7b806b5dc2b6a7a994577d8457dba0148f93dd82677e50ff72ad461a800c4"
-  head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu18.04.AppImage"
+  head "https://github.com/wez/wezterm/releases/download/nightly/WezTerm-nightly-Ubuntu20.04.AppImage"
 
   def install
     Dir.glob("*.AppImage").each do |img|
